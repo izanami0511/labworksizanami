eac96e23d06a281dd7cee134f76c1e4ac0ee05df
izanami0511 <43243390+izanami0511@users.noreply.github.com>
2018-12-24 23:50:30 +0300

    Add files via upload

---
+++ laba10 (2).c
@@ -0,0 +1,79 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	static char text[1000000];
+	char c;
+	int i = 0;
+	int end, start;
+	int counter = 0;
+	FILE *fpin;
+	FILE *fpout;
+	fpin = fopen("input.txt", "rt");
+	if (fpin == NULL)
+	{
+		printf("Error: input.txt\n");
+		return 1;
+	}
+	if ((fpout = fopen("output.txt", "wt")) == NULL)
+	{
+		printf("Error: output.txt\n");
+		return 1;
+	}
+	while (!feof(fpin))
+	{
+		c = fgetc(fpin);
+		if (c == '\n');
+		else
+		{
+			text[i] = c;
+			i++;
+			if (c == '.' || c=='!' || c == '?')
+				text[i++] = '\0';
+		}
+	}
+	counter = i;
+	start = 0;
+	end = 0;
+	for (i = 0; i <= counter; i++)
+	{
+		if (text[i] == '.' || text[i] == '!' || text[i] == '?' || text[i] == '\0')
+		{
+			int k = i;
+			do
+			{
+				end = k;
+				k++;
+			} while (text[k] == '.' || text[k] == '!' || text[k] == '?' || text[k] == ' ' || text[k] == '\0');
+		}
+		else
+		{
+			if (end != 0)
+			{
+
+				char *tmp;
+				char *buf;
+				int k;
+				tmp = (char*)malloc((end - start) * sizeof(char));
+				buf = tmp;
+				for (k = start; k <= end; k++)
+				{
+					*tmp = text[k];
+					++tmp;
+				}
+				fputs(buf, fpout);
+				fputc('\n', fpout);
+				start = end;
+				end = 0;
+			}
+			else continue;
+		}
+
+	}
+	fclose(fpin);
+	fclose(fpout);
+	printf("Program done, check output.txt\n");
+	return 0;
+}
